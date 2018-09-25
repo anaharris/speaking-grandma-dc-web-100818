668cd5630bdf4c346f523493810ddce54f7f4673
Ana Harris <vana1990@gmail.com>
2018-09-25 19:08:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
   else 
+    return ""
   
 
 # Whatever you say to grandma, she should respond with
