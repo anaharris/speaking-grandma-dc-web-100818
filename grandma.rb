b743c7f29a159a59eee680e68440a1f2a45d27a3
Ana Harris <vana1990@gmail.com>
2018-09-25 19:09:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
+def speak_to_grandma (phrase)
   if phrase.uppercase == true
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!" 
